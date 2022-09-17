a0cfa04e49d1a4f640a3077299fc0ad5a4325f49
1212_AMIT <amitpisal1212@yahoo.com>
2022-09-17 17:14:30 +0530

    Added Admin Repository

---
+++ Backend/SocialComplaintProject/src/main/java/demo/socio/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package demo.socio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import demo.socio.entity.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+}
