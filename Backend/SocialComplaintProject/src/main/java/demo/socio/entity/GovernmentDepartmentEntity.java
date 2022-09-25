d5c6420713283b80dc97979ccb865dfbb2aae4a7
PoojaDole <poojadole1000@gmail.com>
2022-09-25 09:55:57 +0530

    updated

--- Backend/SocialComplaintProject/src/main/java/demo/socio/entity/GovernmentDepartmentEntity.java
+++ Backend/SocialComplaintProject/src/main/java/demo/socio/entity/GovernmentDepartmentEntity.java
@@ -29,50 +29,38 @@ public class GovernmentDepartmentEntity {
 
 	@Id
 	@Column(unique = true)
-	@GeneratedValue(strategy = GenerationType.IDENTITY ,generator = "governmment_department_id")
-	private int governmment_department_id;
-
-	@Column
-	private String governmment_department_name;
-
-
-	@OneToMany(mappedBy = "governmment_department_id",cascade =CascadeType.ALL )
-	private List<ComplaintCategoryEntity>department_id=new ArrayList<>();
-
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long government_department_id;
+	
+	private String government_department_name;
+	
+	
 	public GovernmentDepartmentEntity() {
 		super();
 	}
 
-	public List<ComplaintCategoryEntity> getDepartment_id() {
-		return department_id;
-	}
-
-	public void setDepartment_id(List<ComplaintCategoryEntity> department_id) {
-		this.department_id = department_id;
-	}
-
-	public GovernmentDepartmentEntity(
-			int governmment_department_id,
-			String governmment_department_name) {
+	public GovernmentDepartmentEntity(long government_department_id, String government_department_name) {
 		super();
-		this.governmment_department_id = governmment_department_id;
-		this.governmment_department_name = governmment_department_name;
+		this.government_department_id = government_department_id;
+		this.government_department_name = government_department_name;
 	}
 
-	public int getGovernmment_department_id() {
-		return governmment_department_id;
+//	----------------------------------------- GETTERS & SETTERS -----------------------------------------
+	
+	public long getGovernment_department_id() {
+		return government_department_id;
 	}
 
-	public void setGovernmment_department_id(int governmment_department_id) {
-		this.governmment_department_id = governmment_department_id;
+	public void setGovernment_department_id(long government_department_id) {
+		this.government_department_id = government_department_id;
 	}
 
-	public String getGovernmment_department_name() {
-		return governmment_department_name;
+	public String getGovernment_department_name() {
+		return government_department_name;
 	}
 
-	public void setGovernmment_department_name(String governmment_department_name) {
-		this.governmment_department_name = governmment_department_name;
+	public void setGovernment_department_name(String government_department_name) {
+		this.government_department_name = government_department_name;
 	}
 
 
