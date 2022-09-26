8088daaab5bc337e78247cee00d2405df8af5d0b
Nikhil777999 <chandramore.nick777@gmail.com>
2022-09-26 23:26:42 +0530

    Frontend react component files added

---
+++ Frontend/social_complaints/src/dashboard/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>{/* ======= Footer ======= */}
+    <footer id="footer" className="footer">
+      <div className="copyright">
+        &copy; Copyright <strong><span>Admin</span></strong>. All Rights Reserved
+      </div>
+      <div className="credits">
+        {/* All the links in the footer should remain intact. */}
+        {/* You can delete the links only if you purchased the pro version. */}
+        {/* Licensing information: https://bootstrapmade.com/license/ */}
+        {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ */}
+        Designed by <a href="">@2022 C-DAC - IET Group 11</a>
+      </div>
+    </footer>{/* End Footer */}
+    
+    </div>
+  )
+}
